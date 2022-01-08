fa7f802cc96378a89ef493fd8ba7d3300d80af0f
Paul Gangneux <paul.v.gangneux@gmail.com>
2022-01-08 23:57:36 +0100

    la lecture du tube se fait après un poll()

--- src/saturnd.c
+++ src/saturnd.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <inttypes.h>
 #include <sys/stat.h>
+#include <poll.h>
 
 #include "saturnd.h"
 
@@ -57,53 +58,80 @@ int main() {
 }
 
 void saturnd_loop(char* rtp, char* atp) {
-	int request_pipe = open(rtp, O_RDONLY);
-	int answer_pipe = open(atp, O_WRONLY);
+	// le tube est ouvert en lecture-écriture pour éviter que 
+	// poll() retourne immédiatement. il ne faut pas écrire dans ce tube
+	int request_pipe = open(rtp, O_RDWR);
+	int answer_pipe = open(atp, O_WRONLY); 
 
 	task* tasklist; // TODO lire les taches existantes
 	// TODO liste chainee plus adaptee pour liste des taches ?
 
 	// Contenu possible des requetes
 	uint16_t opcode = 0;
+
+	// pour poll request_pipe
+	struct pollfd pfd;
+	pfd.fd = request_pipe;
+	pfd.events = POLLIN;
+
 	while(1) {
 		// TODO execution des taches
 
-		// TODO gestion de toutes les requetes
-		read(request_pipe, &opcode, sizeof(uint16_t));
-		opcode = be16toh(opcode);
+		// on attend une requete pendant 500ms
+		poll(&pfd, 1, 500);
+		// on ne lis et répond à la requête que s'il y en a effectivement une
+		if (pfd.revents & POLLIN) {
+			read(request_pipe, &opcode, sizeof(uint16_t));
+			opcode = be16toh(opcode);
 
-		switch (opcode) {
-			case CLIENT_REQUEST_LIST_TASKS:
-				/* code */
-				break;
-			case CLIENT_REQUEST_CREATE_TASK:
-				/* code */
-				break;
-			case CLIENT_REQUEST_REMOVE_TASK:
-				/* code */
-				break;
-			case CLIENT_REQUEST_GET_TIMES_AND_EXITCODES:
-				/* code */
-				break;
-			case CLIENT_REQUEST_TERMINATE:
-				close(request_pipe);
-				uint16_t rep = htobe16(SERVER_REPLY_OK);
-				write(answer_pipe, &rep, sizeof(uint16_t));
-				close(answer_pipe);
+			switch (opcode) {
+				case CLIENT_REQUEST_LIST_TASKS: {
+					uint32_t nbTasks = 0; //TODO
+					uint16_t rep = htobe16(SERVER_REPLY_OK);
+					nbTasks = htobe32(nbTasks);
+					write(answer_pipe, &rep, sizeof(uint16_t));
+					write(answer_pipe, &nbTasks, sizeof(uint32_t));
 
-				// TODO rm every task folder
-				// TODO free tasklist
-				printf("Exiting");
-				exit(0);
-				break;
-			case CLIENT_REQUEST_GET_STDOUT:
-				/* code */
-				break;
-			case CLIENT_REQUEST_GET_STDERR:
-				/* code */
-				break;
-			default:
-				break;
+					/* code */
+					break;
+				}
+					
+				case CLIENT_REQUEST_CREATE_TASK: {
+					/* code */
+					break;
+				}
+				case CLIENT_REQUEST_REMOVE_TASK: {
+					/* code */
+					break;
+				}
+				case CLIENT_REQUEST_GET_TIMES_AND_EXITCODES: {
+					/* code */
+					break;
+				}
+				case CLIENT_REQUEST_TERMINATE: {
+					close(request_pipe);
+					uint16_t rep = htobe16(SERVER_REPLY_OK);
+					write(answer_pipe, &rep, sizeof(uint16_t));
+					close(answer_pipe);
+
+					// TODO rm every task folder (pas forcément ?)
+					// TODO free tasklist
+					printf("Exiting\n");
+					exit(0);
+					break;
+				}
+				case CLIENT_REQUEST_GET_STDOUT: {
+					/* code */
+					break;
+				}
+				case CLIENT_REQUEST_GET_STDERR: {
+					/* code */
+					break;
+				}
+				default:
+					break;
+			}
+			pfd.revents = 0;
 		}
 	}
 }
