c98a9d9de3e0184c4957c9d77b039afbb4b38f23
Paul Gangneux <paul.v.gangneux@gmail.com>
2022-01-09 03:06:24 +0100

    ajouts de fonctions dans tasklist

--- include/tasklist.h
+++ include/tasklist.h
@@ -17,4 +17,13 @@ typedef struct {
 	task *first;
 } tasklist;
 
+void task_addToTasklist(task *t, tasklist *tl);
+void iter_tasklist(void (*operation)(task *), tasklist *tl);
+task *task_create(uint16_t id, commandline *cmdl, timing timing);
+void task_free(task *t);
+void task_freeAll(task *t);
+int tasklist_length(tasklist *tl);
+tasklist *tasklist_create();
+void tasklist_free(tasklist *tl);
+
 #endif
--- src/tasklist.c
+++ src/tasklist.c
@@ -1,14 +1,14 @@
 #include "tasklist.h"
 
 // ajoute une tache en début de liste
-void task_addToTaskList(task *t, tasklist *tl) {
+void task_addToTasklist(task *t, tasklist *tl) {
 	t->next = tl->first;
 	tl->first = t;
 }
 
 // applique la fonction (task*) -> (void) à toutes les taches
 // de la tasklist
-void iter_tasklist(void (*operation)(task *), tasklist *tl) {
+void tasklist_iter(void (*operation)(task *), tasklist *tl) {
 	task *t = tl->first;
 	while (t != NULL) {
 		operation(t);
@@ -18,6 +18,7 @@ void iter_tasklist(void (*operation)(task *), tasklist *tl) {
 
 // alloue la mémoire necessaire à la création de la tache
 // et renvoie un pointeur vers cette tache
+// on suppose que la mémoire de cmdl a déjà été allouée (mauvaise idée ?)
 task *task_create(uint16_t id, commandline *cmdl, timing timing) {
 	task *task_p = malloc(sizeof(task));
 	task_p->id = id;
@@ -25,6 +26,22 @@ task *task_create(uint16_t id, commandline *cmdl, timing timing) {
 	task_p->timing = timing;
 	task_p->next = NULL;
 	return task_p;
+}
+
+tasklist *tasklist_create() {
+	tasklist *tasklist_p = malloc(sizeof(tasklist));
+	return tasklist_p;
+}
+
+// fonction auxilière
+int length_aux(int n, task* t) {
+	if (t == NULL)
+		return n;
+	return length_aux(n + 1, t->next);
+}
+
+int tasklist_length(tasklist *tl) {
+	return length_aux(0, tl->first);
 }
 
 // free une tache seule, ne free pas t->next
@@ -40,4 +57,9 @@ void task_freeAll(task *t) {
 	task *t2 = t->next;
 	task_free(t);
 	task_freeAll(t2);
+}
+
+void tasklist_free(tasklist *tl) {
+	task_freeAll(tl->first);
+	free(tl);
 }
